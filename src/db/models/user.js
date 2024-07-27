f91d9a57c6f38f389254f92786c876aa6393aa7a
Movsar Makhmutov <m.movsar@gmail.com>
2024-07-27 18:42:54 +0300

    done

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   User.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING // Добавляем поле для пароля
+    password: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'User',
